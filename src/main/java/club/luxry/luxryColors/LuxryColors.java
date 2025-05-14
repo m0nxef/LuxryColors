c0489038e9522dabb79edc80c0d4fbc64ce6c5b7
dz0gg <mahindraaymen@gmail.com>
2025-05-14 09:12:19 -1100

    Initial commit

---
+++ src/main/java/club/luxry/luxryColors/LuxryColors.java
@@ -0,0 +1,17 @@
+package club.luxry.luxryColors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuxryColors extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
